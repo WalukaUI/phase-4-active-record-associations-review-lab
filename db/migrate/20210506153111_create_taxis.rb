a76cc50753449ed468207449a8c182f892b0a17a
WalukaUI <cwaluka@yahoo.com>
2021-09-01 13:41:57 -0700

    completed

--- db/migrate/20210506153111_create_taxis.rb
+++ db/migrate/20210506153111_create_taxis.rb
@@ -1,7 +1,9 @@
 class CreateTaxis < ActiveRecord::Migration[6.1]
   def change
     create_table :taxis do |t|
-
+      t.integer :number
+      t.string :driver_name
+      t.integer :ride_id
       t.timestamps
     end
   end
--- db/migrate/20210506153121_create_passengers.rb
+++ db/migrate/20210506153121_create_passengers.rb
@@ -1,7 +1,9 @@
 class CreatePassengers < ActiveRecord::Migration[6.1]
   def change
     create_table :passengers do |t|
-
+      t.string :name
+      t.string :destination
+      t.integer :ride_id
       t.timestamps
     end
   end
